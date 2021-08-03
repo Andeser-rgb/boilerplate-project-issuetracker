40fa90cc6f47bfd09cb4bafdc081dc9518195bdb
imme <andeser@protonmail.com>
2021-08-03 17:38:39 +0200

    Fixed put errors, probably

--- routes/api.js
+++ routes/api.js
@@ -82,10 +82,16 @@ module.exports = function (app) {
                !issue_text &&
                !created_by &&
                !assigned_to &&
-               !status_text &&
-               !open) res.json({error: "no update filed(s) sent"});
+               !status_text
+              ) {
+                res.json({error: "no update filed(s) sent"});
+                return;
+            }
             ProjectModel.findOne({name: project}, (err, projectData) => {
-                if(!projectData) res.json({error: "could not update", _id: _id});
+                if(!projectData) {
+                    res.json({error: "could not update", _id: _id});
+                    return;
+                }
                 else{
                     const issueData = projectData.issues.id(_id);
                     if(!issueData) res.json({error: "could not update", _id: _id});
@@ -95,11 +101,11 @@ module.exports = function (app) {
                         issueData.created_by = created_by || issueData.created_by;
                         issueData.assigned_to = assigned_to || issueData.assigned_to;
                         issueData.status_text = status_text || issueData.status_text;
-                        issueData.open = open || issueData.open;
+                        issueData.open = open;
                         issueData.updated_on = new Date();
                         projectData.save((err, data) => {
                             if(err || !data) res.json({error: "could not update", _id: _id});
-                            else res.json({result: "successfully updated", _id: _id});
+                            else res.json({result: 'successfully updated', '_id': _id });
                         });
                     }
                 }
